d48632cf9cb268fe64084acc2487c266d3bce4d5
Keith Leung <kla114@asb9700-e03.csil.sfu.ca>
2018-10-26 16:51:03 -0700

    alignment complete

--- AnchorApplication/bounceButton.swift
+++ AnchorApplication/bounceButton.swift
@@ -13,7 +13,7 @@ class bounceButton: UIButton {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
         
-       UIView.animate(withDuration: 0.5, delay: 0, options: .allowUserInteraction, animations: {self.transform = CGAffineTransform.identity}, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 6, options: .allowUserInteraction, animations: {self.transform = CGAffineTransform.identity}, completion: nil)
     }
 }
 
