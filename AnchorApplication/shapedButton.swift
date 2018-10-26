313294c06a3885644df60cf3998d3debe2912c5e
Keith Leung <kla114@asb9700-e03.csil.sfu.ca>
2018-10-25 17:29:22 -0700

    first commit

---
+++ AnchorApplication/shapedButton.swift
@@ -0,0 +1,31 @@
+//
+//  shapedButton.swift
+//  AnchorApplication
+//
+//  Created by Keith Leung on 10/25/18.
+//  Copyright © 2018 Keith Leung. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class shapedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+
+}
