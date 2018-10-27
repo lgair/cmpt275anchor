f8189b68ab6b433d40a8889f6eec90fd568c7763
LJ <lindajang94@hotmail.com>
2018-10-27 16:46:27 -0700

    Finished Decision Survey and added App Icon

---
+++ AnchorApplication/DecisionSurvey.swift
@@ -0,0 +1,25 @@
+//
+//  DecisionSurvey.swift
+//  AnchorApplication
+//
+//  Created by Linda Jang on 2018-10-27.
+//  Copyright © 2018 Keith Leung. All rights reserved.
+//
+
+import UIKit
+
+class DecisionSurvey: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Decision Survey View Loaded");
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
