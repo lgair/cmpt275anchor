ea65f963e52ab75ce134dc5ddea34d720e94f552
lindajang94 <32085043+lindajang94@users.noreply.github.com>
2018-11-17 23:03:51 -0800

    Add files via upload

---
+++ AnchorApplication/GyroscopeAccelerometer.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  IOS-Swift-CoreMotionAccelerometer
+//
+
+import UIKit
+import CoreMotion
+import CoreLocation
+import MapKit
+
+class ViewController: UIViewController , CLLocationManagerDelegate {
+  
+    @IBOutlet weak var xGyro: UILabel!
+    @IBOutlet weak var yGyro: UILabel!
+    @IBOutlet weak var zGyro: UILabel!
+
+    @IBOutlet weak var XAccel: UILabel!
+    @IBOutlet weak var yAccel: UILabel!
+    @IBOutlet weak var zAccel: UILabel!
+    
+    var timer: Timer?
+    var motion = CMMotionManager()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.myGyroscope()
+        self.myAccelerometer()
+        timer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: false, block: { (timer) in
+            self.motion.stopGyroUpdates()
+            self.motion.stopAccelerometerUpdates()
+            print("Timer ended")
+        })
+        
+    }
+
+    
+    func myGyroscope(){
+        print("Start Gyroscope")
+        motion.gyroUpdateInterval = 0.5
+        motion.startGyroUpdates(to: OperationQueue.current!) {
+            (data, error) in
+            print(data as Any)
+            if let trueData =  data {
+                
+                self.view.reloadInputViews()
+                self.xGyro!.text = "x: \(trueData.rotationRate.x)"
+                self.yGyro!.text = "y: \(trueData.rotationRate.y)"
+                self.zGyro!.text = "z: \(trueData.rotationRate.z)"
+            }
+        }
+        return
+    }
+    
+    
+    func myAccelerometer() {
+        print("Start Accelerometer")
+        motion.accelerometerUpdateInterval = 0.5
+        motion.startAccelerometerUpdates(to: OperationQueue.current!) {
+            (data, error) in
+            print(data as Any)
+            if let trueData =  data {
+                
+                self.view.reloadInputViews()
+                self.XAccel!.text = "x: \(trueData.acceleration.x)"
+                self.yAccel!.text = "y: \(trueData.acceleration.y)"
+                self.zAccel!.text = "z: \(trueData.acceleration.z)"
+            }
+        }
+
+        return
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
